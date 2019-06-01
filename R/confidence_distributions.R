98ac21844973c38400bc069f0b0a0d2b21d4ebd4
Denis Infanger <7227552+DInfanger@users.noreply.github.com>
2019-06-01 13:32:35 +0200

    Fixed an error with xlimits.

--- R/confidence_distributions.R
+++ R/confidence_distributions.R
@@ -827,27 +827,11 @@ conf_dist <- function(
 
   }
 
-  if (plot_type %in% c("p_val", "s_val", "cdf") && !is.null(conf_level)) {
-
-    hlines_tmp <- text_frame$p_value
-
-    if (plot_type %in% "s_val") {
-      hlines_tmp <- -log2(hlines_tmp)
-    }
-
-    p <- p + geom_hline(yintercept = hlines_tmp, linetype = 2)
-
-  } else if (plot_type %in% c("pdf", "cdf")) {
-    p <- p + scale_y_continuous(breaks = scales::pretty_breaks(n = 10))
-  }
-
-  if (!is.null(null_values)) {
-    p <- p + geom_vline(data = res$counternull_frame, aes(xintercept = null_value), linetype = 1, size = 0.5)
-  }
-
   if (trans %in% "exp" && plot_type %in% "p_val") {
 
-    curr_x_limits <- ggplot_build(p)$layout$panel_params[[1]]$x.range
+    xlim_new <- NA
+
+    # curr_x_limits <- ggplot_build(p)$layout$panel_params[[1]]$x.range
 
     p <- p + scale_x_continuous(trans = "log", breaks = scales::pretty_breaks(n = 10))
 
@@ -855,7 +839,19 @@ conf_dist <- function(
       p <- p + annotate("rect", xmin=0, xmax=100, ymin=ifelse(alternative %in% "two_sided", plot_p_limit, plot_p_limit*2), ymax=cut_logyaxis, alpha=0.1, colour = grey(0.9))
     }
 
-    p <- p + coord_cartesian(xlim = c(min(curr_x_limits, xlim), max(curr_x_limits, xlim)), expand = TRUE)
+    if (!is.null(null_values_trans) && xlim[1] <= min(null_values_trans)) {
+      xlim_new[1] <- xlim[1]
+    } else if (!is.null(null_values_trans) && xlim[1] > min(null_values_trans)) {
+      xlim_new[1] <- min(res$res_frame$values, na.rm = TRUE)
+    }
+
+    if (!is.null(null_values_trans) && xlim[2] >= max(null_values_trans)) {
+      xlim_new[2] <- xlim[2]
+    } else if (!is.null(null_values_trans) && xlim[2] < max(null_values_trans)) {
+      xlim_new[2] <- max(res$res_frame$values, na.rm = TRUE)
+    }
+
+    p <- p + coord_cartesian(xlim = xlim_new, expand = TRUE)
 
     if (!is.null(hlines_tmp)) {
       p <- p + geom_hline(yintercept = hlines_tmp, linetype = 2)
@@ -864,11 +860,13 @@ conf_dist <- function(
   } else if (trans %in% "exp" && plot_type %in% c("cdf", "pdf", "s_val")){
 
     p <- p +
+      # coord_cartesian(xlim = do.call(trans, list(x = xlim)), ylim = limit) +
       scale_x_continuous(trans = "log", breaks = scales::pretty_breaks(n = 10), limits = xlim)
 
   } else {
 
     p <- p +
+      # coord_cartesian(xlim = do.call(trans, list(x = xlim)), ylim = limit) +
       scale_x_continuous(breaks = scales::pretty_breaks(n = 10), limits = xlim)
 
   }
